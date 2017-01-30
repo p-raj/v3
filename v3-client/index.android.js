42aa14ba22291214e16e6878777a96b682f4d05a
Peeyush <praj@teramatrix.in>
2017-01-30 11:39:52 +0530

    remove the folder, add a proper link to the repo

--- v3-client/index.android.js
+++
@@ -1,5 +0,0 @@
-import {AppRegistry} from "react-native";
-import App from "./src/App";
-
-
-AppRegistry.registerComponent('veris_client', () => App);
--- v3-client/src/common/constants.js
+++
@@ -1,4 +0,0 @@
-export const FAILED = -1;
-export const PENDING = 0;
-export const START = 1;
-export const SUCCESS = 2;
